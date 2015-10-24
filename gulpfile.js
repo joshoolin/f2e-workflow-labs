1a00aba307a81e34bf1920182d9e5695b9e7ede5
Joshua Lin <joshoolin@gmail.com>
2015-10-24 15:18:19 +0800

    10 建立與設定 mytask1, mytask2 工作相依於 default 工作 11 在 mytask1 的 fn 加入 "cb" 參數，並在 fn 最後執行 cb(); 12 在 mytask2 的 fn 加入 "cb" 參數，並在 fn 最後執行 cb(); 13 在 mytask1 的 fn 加入 "cb" 參數，並在 fn 最後將 cb(); 註解，執行 default 工作，確認執行順序

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 var gulp = require('gulp');
 
-gulp.task('default', function () {
+gulp.task('default', ['myTask1', 'myTask2'], function () {
 	// place code for your default task here
 	console.log('Hello, Gulp!');
+});
+
+gulp.task('myTask1', function (done) {
+	console.log('Hello task1');
+	// done();
+});
+
+gulp.task('myTask2', function (done) {
+	console.log('Hello task2');
+	done();
 });
