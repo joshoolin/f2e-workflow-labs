90ca0eebf88bc15a61e2af67b5cac6b262335224
Joshua Lin <joshoolin@gmail.com>
2015-10-24 15:37:25 +0800

    14 建立 output1 工作，將所有 assets/vendor/bootstrap/**/*.js 檔案全部輸出到 output1 資料夾下 15 建立 output2 工作，將所有 assets/vendor/bootstrap/**/*.js 檔案全部輸出到 output2 資料夾下，輸出的檔案必須包含 bootstrap 資料夾 16 建立 output3 工作，將所有 assets/vendor/ 目錄下所有 *.js 與 *.css 全部輸出到 output3 資料夾下

--- gulpfile.js
+++ gulpfile.js
@@ -14,3 +14,27 @@ gulp.task('myTask2', function (done) {
 	console.log('Hello task2');
 	done();
 });
+
+gulp.task('output1', function () {
+	gulp
+		.src('assets/vendor/bootstrap/**/*.js')
+		.pipe(gulp.dest('output1'));
+});
+
+gulp.task('output2', function () {
+	gulp
+		.src('assets/vendor/bootstrap/**/*.js',
+			{
+				base: 'assets/vendor/'
+			})
+		.pipe(gulp.dest('output2'));
+});
+
+gulp.task('output3', function () {
+	gulp
+		.src([
+			'assets/vendor/bootstrap/**/*.js',
+			'assets/vendor/bootstrap/**/*.css'
+		])
+		.pipe(gulp.dest('output3'));
+});
